0ac8d82b6d44590a6b7a272176a6638be573005a
Tal Tal <vothanhlam1793@gmail.com>
2024-09-24 17:31:57 +0700

    update fix

--- app/route/index.js
+++ app/route/index.js
@@ -1,88 +1,91 @@
-var express = require('express');
-function redirectLogin(req, res, next){
-    if(req.session.keystoneItemId){
-        next();
-    } else {
-        res.redirect("/login");
-    }
+var express = require('express')
+function redirectLogin(req, res, next) {
+  if (req.session.keystoneItemId) {
+    next()
+  } else {
+    res.redirect('/login')
+  }
 }
 
-module.exports = (keystone) => {
-    var router = express.Router();
-    router.get("/", redirectLogin, (req, res)=>{
-        // console.log(keystone);
-        res.render("homepage");
-    })
-    
-    router.get("/login", (req, res)=>{
-        res.render("login");
-    })
+module.exports = keystone => {
+  var router = express.Router()
+  router.get('/', redirectLogin, (req, res) => {
+    // console.log(keystone);
+    res.render('homepage')
+  })
 
-    router.get("/error", (req, res)=>{
-        res.render("error");
-    })
+  router.get('/test', (req, res) => {
+    res.render('test')
+  })
+  router.get('/login', (req, res) => {
+    res.render('login')
+  })
 
-    router.get("/checkChangePassWord", redirectLogin, async (req, res)=>{
-        QUERY_USER = `
+  router.get('/error', (req, res) => {
+    res.render('error')
+  })
+
+  router.get('/checkChangePassWord', redirectLogin, async (req, res) => {
+    QUERY_USER = `
             query getUser($id: ID!){
                 User(where: { id: $id }) {
                     name
                     state
                 }
             }
-        `;
-        var c = await keystone.executeGraphQL({
-            context: keystone.createContext({skipAccessControl: true}),
-            query: QUERY_USER,
-            variables: {
-                id: req.session.keystoneItemId
-            }
-        });
-        res.send(c);
-    });
+        `
+    var c = await keystone.executeGraphQL({
+      context: keystone.createContext({ skipAccessControl: true }),
+      query: QUERY_USER,
+      variables: {
+        id: req.session.keystoneItemId
+      }
+    })
+    res.send(c)
+  })
 
-    router.post("/changePassword", redirectLogin, async(req, res)=>{
-        var CHANGE_PASSWORD = `        
+  router.post('/changePassword', redirectLogin, async (req, res) => {
+    var CHANGE_PASSWORD = `        
         mutation changePassword($id: ID!, $password: String!, $state: String!){
             updateUser(id: $id, data: { password: $password, state: $state }) {
                 id
             }
         }
         `
-        var c = await keystone.executeGraphQL({
-            context: keystone.createContext({skipAccessControl: true}),
-            query: CHANGE_PASSWORD,
-            variables: {
-                id: req.session.keystoneItemId,
-                password: req.body.pwd,
-                state: "NORMAL"
-            }
-        });
-        res.send(c);
-    });
-
-    router.get("/checkAuth", (req, res)=>{
-        if(req.session.keystoneItemId){
-            res.send({
-                auth: true
-            })
-        } else {
-            res.send({
-                auth: false
-            })
-        }
+    var c = await keystone.executeGraphQL({
+      context: keystone.createContext({ skipAccessControl: true }),
+      query: CHANGE_PASSWORD,
+      variables: {
+        id: req.session.keystoneItemId,
+        password: req.body.pwd,
+        state: 'NORMAL'
+      }
     })
+    res.send(c)
+  })
 
-    router.get("/info", (req, res)=> {
-        res.render("info.ejs");
-    });
+  router.get('/checkAuth', (req, res) => {
+    if (req.session.keystoneItemId) {
+      res.send({
+        auth: true
+      })
+    } else {
+      res.send({
+        auth: false
+      })
+    }
+  })
 
-    router.get("/logout", (req, res)=> {
-        res.render("logout.ejs");
-    });
+  router.get('/info', (req, res) => {
+    res.render('info.ejs')
+  })
 
-    router.get("/cameras", redirectLogin, async (req, res)=>{
-        var QUERY_CAMERAS = `
+  router.get('/logout', (req, res) => {
+    res.render('logout.ejs')
+  })
+
+  router.get('/cameras', redirectLogin, async (req, res) => {
+    var QUERY_CAMERAS = `
         query getLopHocByUser($id: ID!){
             User(where: {id: $id}){
               name
@@ -111,20 +114,20 @@ module.exports = (keystone) => {
             }
         }
         `
-        var c = await keystone.executeGraphQL({
-            context: keystone.createContext({skipAccessControl: true}),
-            query: QUERY_CAMERAS,
-            variables: {
-                id: req.session.keystoneItemId,
-            }
-        });
-        res.send(c);
+    var c = await keystone.executeGraphQL({
+      context: keystone.createContext({ skipAccessControl: true }),
+      query: QUERY_CAMERAS,
+      variables: {
+        id: req.session.keystoneItemId
+      }
     })
-    router.get("/manage", function(req, res){
-        res.render("manage/index");
-    })
-    router.get("/mNotify", function(req, res){
-        res.render("manage/notify");
-    })
-    return router;
-};
+    res.send(c)
+  })
+  router.get('/manage', function (req, res) {
+    res.render('manage/index')
+  })
+  router.get('/mNotify', function (req, res) {
+    res.render('manage/notify')
+  })
+  return router
+}
